91a648d411159d50e39577cd6d4281d29cd2a291
dvydinh <doanvy.dinh27@gmail.com>
2025-07-29 18:08:28 +0700

    init loss functions package

---
+++ raml/losses/__init__.py
@@ -0,0 +1,4 @@
+from raml.losses.center_loss import CenterLoss
+from raml.losses.margin_loss import AdaptiveMarginLoss, InputSizeConditionedMarginLoss
+from raml.losses.contrastive_loss import SupervisedContrastiveLoss
+from raml.losses.combined_loss import CompositeAnomalyLoss, MACCLLoss
